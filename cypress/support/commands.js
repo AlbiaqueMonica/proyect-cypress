b6e42523b289e510f720e5c4de321a5c316b1162
AlbiaqueMonica <monic.albiaque@gmail.com>
2022-09-29 22:12:33 -0300

    US-GX-641-ToolsQA | Elements | Text Box: Fill form and Submit

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,7 +8,31 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
+
+//Commands para el componente Element|TextBox para Email inválido
+
+Cypress.Commands.add("NoEnvíoDeFormularioMailInvalido", (name, email, address, permAddress)=>{
+
+        // This field is invalid when:
+		// Does not contain “@”
+		// Does not contain (minimum) 1 alphanumeric character before “@”
+		// Does not contain (minimum) 1 alphanumeric character after “@”
+		// Does not contain “.” after: 1 alphanumeric character after “@”.
+		// Does not contain (minimum) 2 alphanumeric characters after “.”
+		//Mockup: “x@x.xx”
+		cy.fixture("DOM/toolsqa/Elements/TextBox.Page").then((the) =>
+		{
+			cy.get(the.name.input).type(name)
+			cy.get(the.email.input).type(email).click({force: true})
+			cy.get(the.currentAd.input).type(address)
+            cy.get(the.permanentAd.input).type(permAddress)
+            cy.get(the.SubmitBtn).click()
+            cy.get(the.Submit.Fail).should('be.visible')
+            cy.get('.border').should('not.exist')
+	})
+})
+
+
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
